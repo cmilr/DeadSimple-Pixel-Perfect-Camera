8baa79a4b517507ff21601415ea06ee93609543f
Cary Miller <cary.a.miller@gmail.com>
2017-07-31 15:27:44 -0700

    Add validation of user input
    Script was experiencing a 'divide by zero' error, so I added some simple
    validation to test for zero in the user's input, and if necessary reset
    to the minimum valid value, which is one.

--- Assets/DSPixelPerfectCamera.cs
+++ Assets/DSPixelPerfectCamera.cs
@@ -1,33 +1,42 @@
-﻿using UnityEngine;
+﻿
+// Usage: In the inspector, punch in your desired settings and then 
+// enter PLAY mode to apply.
+
+using UnityEngine;
 using UnityEngine.Assertions;
 
+[ExecuteInEditMode]
 public class DSPixelPerfectCamera : MonoBehaviour
 {
 	public float FinalUnitSize 		{ get { return finalUnitSize; } }
 	public int   PixelsPerUnit 		{ get { return pixelsPerUnit; } }
 	public int   VertUnitsOnScreen 	{ get { return verticalUnitsOnScreen; } }
 
+
 	[SerializeField]
-	private int pixelsPerUnit;
+	private int pixelsPerUnit = 16;
 	[SerializeField]
-	private int verticalUnitsOnScreen;
+	private int verticalUnitsOnScreen = 4;
 	private float finalUnitSize;
 	private new Camera camera;
+
 
 	void Awake()
 	{
 		camera = gameObject.GetComponent<Camera>();
 		Assert.IsNotNull(camera);
 
-		SetOrthographicSize(verticalUnitsOnScreen);
+		SetOrthographicSize();
 	}
 
-	void SetOrthographicSize(int unitsOnScreen)
+	void SetOrthographicSize()
 	{
+		ValidateUserInput();
+
 		// get device's screen height and divide by the number of units 
 		// that we want to fit on the screen vertically. this gets us
 		// the basic size of a unit on the the current device's screen.
-		var tempUnitSize = Screen.height / unitsOnScreen;
+		var tempUnitSize = Screen.height / verticalUnitsOnScreen;
 
 		// with a basic rough unit size in-hand, we now round it to the
 		// nearest power of pixelsPerUnit (ex; 16px.) this will guarantee
@@ -50,5 +59,21 @@ public class DSPixelPerfectCamera : MonoBehaviour
 			result += multiple;
 
 		return result;
+	}
+
+	void ValidateUserInput()
+	{
+		if (pixelsPerUnit == 0)
+		{
+			pixelsPerUnit = 1;
+			Debug.Log("Warning: Pixels-per-unit must be greater than zero. " +
+			          "Resetting to minimum allowed.");
+		}
+		else if (verticalUnitsOnScreen == 0)
+		{
+			verticalUnitsOnScreen = 1;
+			Debug.Log("Warning: Units-on-screen must be greater than zero." +
+			          "Resetting to minimum allowed.");
+		}
 	}
 }
